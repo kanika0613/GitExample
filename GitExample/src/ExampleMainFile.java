afc10c14c771f251bb095d3d8566798fec0344e5
TechM user <TechM user@Kanika>
2018-03-24 15:23:25 -0700

    SYSOUT change

--- GitExample/src/ExampleMainFile.java
+++ GitExample/src/ExampleMainFile.java
@@ -3,6 +3,8 @@ public class ExampleMainFile {
 
 	public static void main(String[] args) {
 		System.out.println("This is main file");
+		
+		System.out.println("This is main file...New Line");
 	}
 
 }
